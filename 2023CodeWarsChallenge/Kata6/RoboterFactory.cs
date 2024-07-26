21767196e16123170be3326842b90f3712836ee7
Emogibam <88818922+Emogibam@users.noreply.github.com>
2024-07-26 06:45:23 +0100

    adding RoboterFactory

---
+++ 2023CodeWarsChallenge/Kata6/RoboterFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023CodeWarsChallenge.Kata6
+{
+    public class RoboterFactory
+    {
+        //https://www.codewars.com/kata/5898a7208b431434e500013b/train/csharp
+        public long CalculateScrap(int[] scraps, int numberOfRobots)
+        {
+            decimal totalIronNeeded = numberOfRobots * 50m;
+
+            for (int i = scraps.Length - 1; i >= 0; i--)
+            {
+                decimal efficiency = (100m - scraps[i]) / 100m;
+                totalIronNeeded = totalIronNeeded / efficiency;
+            }
+
+            return (long)Math.Ceiling(totalIronNeeded);
+        }
+    }
+}
